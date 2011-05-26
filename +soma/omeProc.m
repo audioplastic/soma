1bc83868131c12480d44b1726e620eb94825199c
Nick Clark <nrclark@essex.ac.uk>
2011-05-26 17:06:35 +0100

    first commit

---
+++ +soma/omeProc.m
@@ -0,0 +1,56 @@
+classdef omeProc < soma.sigProc
+    %OMEPROC Outer middle ear processing
+    %   Very simplified version of oute rmiddle ear transfer function. Just
+    %   a butterworth filter.
+
+    properties
+        %OUT/MID EAR PROPERTIES
+        ome_fLoCut  = 450;  %lf cutoff (default = 450 Hz)
+        ome_fHiCut  = 8000; %hf cutoff (default = 8000 Hz)
+        ome_fOrd    = 2;    %filter order (default = 2nd)
+    end
+
+    %************************************************************
+    % Dependents - never set by user, only calculated when needed
+    %************************************************************
+    properties (Dependent = true)
+        ome %Signal post ome processing
+    end
+
+    methods
+        %% **********************************************************
+        % Constructor
+        %************************************************************
+        function obj = omeProc(varargin)
+            obj = obj@soma.sigProc(varargin{:});
+        end
+        
+        %% **********************************************************
+        % Outer/mid ear processing -> get method
+        %************************************************************
+        function value = get.ome(obj)
+            [midEar_b,midEar_a] = butter(obj.ome_fOrd,...
+                [obj.ome_fLoCut obj.ome_fHiCut]*2*(1/obj.sr)); %Middle ear transfer coefficients
+            value = filter(midEar_b,midEar_a,obj.sig);
+        end
+
+        %% **********************************************************
+        % Set methods - dull error catching code
+        %************************************************************
+        function obj = set.ome_fHiCut(obj,value)
+            assert (value < obj.sr/2 && value > 0,...
+                'fMax must be +ve and below Nyquist limit ( < sr/2 )')
+            obj.ome_fHiCut = value;
+        end
+        function obj = set.ome_fLoCut(obj,value)
+            assert (value <= obj.ome_fHiCut && value > 0,...
+                'fMin must be +ve and below fMax')
+            obj.ome_fLoCut = value;
+        end
+        function obj = set.ome_fOrd(obj,value)
+            assert (value > 0 && ~mod(value,1),...
+                'fOrd must be a positive integer')
+            obj.ome_fOrd = value;
+        end
+    end
+end
---
+++ +soma/sigProc.m
@@ -0,0 +1,84 @@
+classdef sigProc
+    %SIGPROC Simple signal class
+    %   Superclass for everything. Includes a few simple properties such as
+    %   duration calculation etc.
+
+    properties
+        sr           = 25e3
+        sig          = repmat([1 zeros(1,499)],1,5)
+    end
+    %************************************************************
+    % Dependents - never set by user, only calculated when needed
+    %************************************************************
+    properties (Dependent = true)
+        tAxis
+        nSamp
+        dur
+    end
+
+    methods
+        %% **********************************************************
+        % Constructor - can pass audio signal on instantiation
+        %************************************************************
+        function obj = sigProc(ip,sr)
+            if nargin > 0
+                obj.sig = ip;
+            end
+            if nargin > 1
+                obj.sr = sr;
+            end
+%             obj.Splash
+        end
+
+        %% **********************************************************
+        % Overloaded functions
+        %************************************************************
+        function soundsc(obj)
+            soundsc(obj.sig,obj.sr);
+        end
+        function sound(obj)
+            sound(obj.sig,obj.sr);
+        end
+        
+        %% **********************************************************
+        % Utility get methods
+        %************************************************************
+        function value = get.nSamp(obj)
+            value = length(obj.sig);
+        end
+        function value = get.dur(obj)
+            value = obj.nSamp/obj.sr;
+        end
+        function value = get.tAxis(obj)
+            value = 1/obj.sr:1/obj.sr:obj.dur;
+        end
+       
+        %% ***********************************************************
+        % Set methods - dull error catching code
+        %************************************************************
+        function obj = set.sr(obj,value)
+            assert (value > 0,...
+                'Sampling rate must be positive')
+            obj.sr = value;
+        end
+        function obj = set.sig(obj,value)
+            assert (size(value,1) < 2 ,...
+                'Audio signal must be input as a mono row-vector (use a second soma object for stereo)')
+            obj.sig = value;
+        end
+               
+    end
+    %************************************************************
+    % Static method Easter Egg
+    %************************************************************
+    methods (Static)
+        function Splash
+            splTmp =[   '                __    __               \n' ...
+                ' _ __  _ __ ___/ / /\\ \\ \\__ _ _ __ ___ ™\n' ...
+                '| ''_ \\| ''__/ __\\ \\/  \\/ / _` | ''__/ _ \\ \n' ...
+                '| | | | | | (__ \\  /\\  / (_| | | |  __/\n' ...
+                '|_| |_•_|• \\___| \\/  \\/ \\__,_|_|  \\___| Nick Clark 2009\n\n\n'   ];
+            fprintf(1,splTmp);
+        end%Splash
+    end%static methods
+end
